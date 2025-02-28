546645def21493d3aa95be3e65e65fd611e72f2a
AeroBytes <kelvin@aerobytes.nl>
2025-02-28 01:44:57 +0100

    Added margin top to footer

--- apps/web/components/footer.tsx
+++ apps/web/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-      <footer className="sticky bottom-0 left-0 right-0  p-4 text-center text-white" style={{ backgroundColor: "#069AF3" }}>
+      <footer className="sticky bottom-0 left-0 right-0 m p-4 text-center text-white" style={{ backgroundColor: "#069AF3", marginTop: "100px" }}>
         <p>Brought to you by <a href={"https://aerobytes.nl"} target={"_blank"}>AeroBytes</a></p>
         <small>
           Credits to <a href="https://github.com/arikchakma/maily.to" target={"_blank"}>Maily.to</a>
